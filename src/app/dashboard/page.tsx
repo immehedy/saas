861950b0696d83efb4190bdc8ae4e2d1b31823e0
mehedy hassan <mehedy@algosolver.com>
2023-08-16 13:33:40 +0600

    saas stripe log usage show complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import {
   createCheckoutLink,
   createCustomerIfNull,
   hasSubscriptions,
+  stripe,
 } from "@/lib/stripe";
 import Link from "next/link";
 import { PrismaClient } from "@prisma/client";
@@ -20,6 +21,28 @@ const page = async () => {
       email: session?.user?.email,
     },
   });
+
+  const top10RecentLogs = await prisma?.log?.findMany({
+    where: {
+      userId: user?.id,
+    },
+    orderBy: {
+      created: "desc",
+    },
+    take: 10,
+  });
+
+  let curr_usage = 0;
+
+  if(hasSub){
+    const subscriptions = await stripe?.subscriptions?.list({
+        customer: String(user?.stripe_customer_id),
+      });
+      const invoice = await stripe?.invoices?.retrieveUpcoming({
+        subscription: subscriptions?.data?.at(0)?.id
+      })
+      curr_usage = invoice?.amount_due;
+  }
   return (
     <main>
       {hasSub ? (
@@ -28,8 +51,34 @@ const page = async () => {
             You have a subscription !
           </div>
           <div className="divide-y divide-zinc-200 border border-zinc-200 rounded-md">
+            <p className="text-sm text-black px-6 py-4 font-medium">Current Usage</p>
+            <p className="text-sm font-mono text-zinc-800 px-6 py-4">
+              ${curr_usage/100}
+            </p>
+          </div>
+          <div className="divide-y divide-zinc-200 border border-zinc-200 rounded-md">
             <p className="text-sm text-black px-6 py-4 font-medium">Api Key</p>
-            <p className="text-sm font-mono text-zinc-800 px-6 py-4">{user?.api_key}</p>
+            <p className="text-sm font-mono text-zinc-800 px-6 py-4">
+            <Link href={`/api/endpoint?api_key=${user?.api_key}`} rel="noopener noreferrer" target="_blank" className="hover:opacity-50 hover:underline">
+              {user?.api_key}
+            </Link>
+            </p>
+          </div>
+          <div className="divide-y divide-zinc-200 border border-zinc-200 rounded-md">
+            <p className="text-sm text-black px-6 py-4 font-medium">Logs</p>
+            {top10RecentLogs?.map((item, index) => (
+              <div className="flex items-center gap-4" key={index}>
+                <p className="text-sm font-mono text-zinc-800 px-6 py-4">
+                  {item?.method}
+                </p>
+                <p className="text-sm font-mono text-zinc-800 px-6 py-4">
+                  {item?.status}
+                </p>
+                <p className="text-sm font-mono text-zinc-800 px-6 py-4">
+                  {item?.created?.toDateString()}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       ) : (
